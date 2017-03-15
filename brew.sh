9d5ddcdf75b0875391e39d5df7d3641fff9b3420
Ndagi Stanley <ndagis@gmail.com>
2017-03-15 06:10:02 +0100

    Tested and tried

---
+++ brew.sh
@@ -0,0 +1,18 @@
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+brew install zsh
+brew install zsh-syntax-highlighting
+
+brew install heroku
+brew install hub
+brew install ctags
+brew install httpie
+brew install tree
+brew install git
+
+# Remove outdated versions from the cellar.
+brew cleanup
---
+++ cask.sh
@@ -0,0 +1,9 @@
+# Fonts
+brew tap caskroom/fonts 2> /dev/null
+brew cask install font-source-code-pro 2> /dev/null
+
+brew cask install docker 2> /dev/null
+brew cask install ngrok 2> /dev/null
+brew cask install slack 2> /dev/null
+brew cask install viscosity 2> /dev/null
+brew cask install vlc 2> /dev/null
