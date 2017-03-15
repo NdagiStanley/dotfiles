137d684f9a7f5f3f8c81011b97d0d0c0c3dd1736
Ndagi Stanley <ndagis@gmail.com>
2017-03-15 10:22:52 +0100

    Edit cask.sh

--- cask.sh
+++ cask.sh
@@ -4,6 +4,8 @@ brew cask install font-source-code-pro 2> /dev/null
 
 brew cask install docker 2> /dev/null
 brew cask install ngrok 2> /dev/null
+brew cask install pomello 2> /dev/null
 brew cask install slack 2> /dev/null
 brew cask install viscosity 2> /dev/null
 brew cask install vlc 2> /dev/null
+brew cask install whatsapp 2> /dev/null
