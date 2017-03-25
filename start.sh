91e9ee5cc80c5098edcd4b3f329af435b9155d92
Stanley Ndagi <NdagiStanley@users.noreply.github.com>
2017-03-25 15:49:39 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ vim +PluginInstall +qall
 
 # Zsh
 git clone https://github.com/NdagiStanley/rafiki-zsh.git
-cp rafiki-zsh/rafiki.zsh-theme ~/.oh-my-zsh/custom/themes/
+cp rafiki-zsh/rafiki.zsh-theme ~/.oh-my-zsh/custom/themes
 vim ~/.zshrc
 # ZSH_THEME='rafiki'
 
